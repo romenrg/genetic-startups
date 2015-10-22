fcd848def89d84ebda2476a825f8c8c0ae3687df
Romén Rodríguez <romenrg@gmail.com>
2015-10-22 11:52:09 +0100

    Adding a new icon, for "product release"

--- GeneticStartupsWindows/GeneticStartupsWindows/Properties/Resources.Designer.cs
+++ GeneticStartupsWindows/GeneticStartupsWindows/Properties/Resources.Designer.cs
@@ -143,6 +143,16 @@ namespace GeneticStartupsWindows.Properties {
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap product_release {
+            get {
+                object obj = ResourceManager.GetObject("product_release", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap startups_circus {
             get {
                 object obj = ResourceManager.GetObject("startups_circus", resourceCulture);
